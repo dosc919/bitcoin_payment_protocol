57cf164d67e7273bede6787a65a5841501efeacf
Dominik Schreilechner <schreilechner@student.tugraz.at>
2019-02-26 11:57:03 +0100

    corrected error in gitignore

--- daps/cmake-build-debug/test/lib/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dominik/projects/2017_master_schreilechner/code/daps/test/lib/googletest/googletest
-# Build directory: /home/dominik/projects/2017_master_schreilechner/code/daps/cmake-build-debug/test/lib/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
